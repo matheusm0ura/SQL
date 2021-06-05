77d593430527c345978f104d10a90d634f9841ee
matheusm0ura <53587013+matheusm0ura@users.noreply.github.com>
2021-06-05 00:32:08 -0300

    Update w3resource_JOIN.sql

--- Exercícios/w3resource_JOIN.sql
+++ Exercícios/w3resource_JOIN.sql
@@ -1,3 +1,10 @@
+
+/*28. Write a query in SQL to find the first name and last name of employees working for departments with a budget more than Rs. 50000. */
+
+SELECT e.EMP_FNAME, e.EMP_LNAME FROM emp_department AS d 
+JOIN emp_details AS e ON d.DPT_CODE = e.EMP_DEPT
+WHERE d.DPT_ALLOTMENT > 50000;
+
 /*29. Write a query in SQL to find the names of departments where more than two employees are working.*/
 
 SELECT d.DPT_NAME FROM emp_department AS d 
