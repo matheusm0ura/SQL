fdbd098990a65e46b0523fbc75aed9d809939c10
matheusm0ura <53587013+matheusm0ura@users.noreply.github.com>
2021-06-04 23:49:12 -0300

    Update and rename w3resource.sql to w3resource_JOIN.sql

--- Exercícios/w3resource_JOIN.sql
+++ Exercícios/w3resource_JOIN.sql
@@ -1,4 +1,4 @@
-29. Write a query in SQL to find the names of departments where more than two employees are working.
+/*29. Write a query in SQL to find the names of departments where more than two employees are working.*/
 
 SELECT d.DPT_NAME FROM emp_department AS d 
 JOIN emp_details AS e ON d.DPT_CODE = e.EMP_DEPT
