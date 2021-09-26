acd8b10192f7a54d3b37b171774207ddfa8faa14
matheusm0ura <53587013+matheusm0ura@users.noreply.github.com>
2021-09-25 22:43:03 -0300

    Update EX003.sql

--- Exercícios/EX003.sql
+++ Exercícios/EX003.sql
@@ -5,3 +5,11 @@ SELECT * FROM salesman
 /*2. Write a SQL statement to display a string "This is SQL Exercise, Practice and Solution"*/
 
 SELECT 'This is SQL Exercise, Practice and Solution';
+
+/*32. From the following table, write a SQL query to find the details of employees whose last name is 'Snares'. Return emp_idno, emp_fname, emp_lname, and emp_dept.*/
+
+SELECT * FROM emp_details WHERE EMP_LNAME = 'Snares';
+
+/*33. From the following table, write a SQL query to find the details of the employees who work in the department 57. Return emp_idno, emp_fname, emp_lname and emp_dept.*/
+
+SELECT emp_idno, emp_fname, emp_lname, emp_dept FROM emp_details WHERE EMP_DEPT = 57;
