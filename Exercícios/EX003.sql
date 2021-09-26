a87573eeb918b89b54b73a95c1db1303360615ea
matheusm0ura <53587013+matheusm0ura@users.noreply.github.com>
2021-09-25 22:30:43 -0300

    Update EX003.sql

--- Exercícios/EX003.sql
+++ Exercícios/EX003.sql
@@ -1,2 +1,7 @@
-01 - Write a SQL statement to display all the information of all salesmen.
+/*1. Write a SQL statement to display all the information of all salesmen. */
+
 SELECT * FROM salesman
+
+/*2. Write a SQL statement to display a string "This is SQL Exercise, Practice and Solution"*/
+
+SELECT 'This is SQL Exercise, Practice and Solution';
